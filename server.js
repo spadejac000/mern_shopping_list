5ac0c12d54ab28fae69d07748840a710bbdea0eb
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-09-22 09:49:00 -0700

    set up back end

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); 
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // Bodyparser Middleware
@@ -12,9 +14,12 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
 mongoose
-     .connect(db)
+    .connect(db)
     .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
+
+// Use Routes
+app.use('api/items',  items);
 
     const port = process.env.PORT || 5000;
 
