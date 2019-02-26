78f7e2959f060d6e0001cbfd625f0d3f1486fad5
Jacob Spade <spade.jacob@Yahoo.com>
2019-02-26 14:30:59 -0800

    fixed redux store

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = require('./config/keys').mongoURI;
 // Connect to Mongo
 mongoose
     .connect(db)
-    .then(() => console.log('MongoDB Connected'))
+    .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
 // Use Routes
