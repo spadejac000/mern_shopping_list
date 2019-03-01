5c93ce68e7f7f54c2be5378666a6f2fb774fb7eb
Jacob Spade <spade.jacob@Yahoo.com>
2019-03-01 12:53:00 -0800

    created delete action in redux

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap';
+
+class ItemModal extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ItemModal;
