86f4770aad13bf865a88e3b4de1ffbe69f0a6482
Jacob Spade <spade.jacob@Yahoo.com>
2019-03-02 14:36:10 -0800

    added axios post request

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -11,17 +11,23 @@ export const getItems = () => dispatch => {
     }))
 };
 
+export const addItem = item => dispatch => {
+  // return {
+  //   type: ADD_ITEM,
+  //   payload: item
+  // }
+  axios
+    .post('http://localhost:5000', item)
+    .then(res => dispatch({
+      type: ADD_ITEM,
+      payload: res.data
+    }))
+}
+
 export const deleteItem = id => {
   return {
     type: DELETE_ITEM,
     payload: id
-  }
-}
-
-export const addItem = (item) => {
-  return {
-    type: ADD_ITEM,
-    payload: item
   }
 }
 
